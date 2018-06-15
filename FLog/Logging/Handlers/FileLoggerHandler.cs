7b78f738853942f7d0ec762c2af2fe4df8045d3d
Tristen Horton <tristen@tristenhorton.com>
2018-06-15 11:19:58 -0700

    We require write permission and allows readwrite

--- FLog/Logging/Handlers/FileLoggerHandler.cs
+++ FLog/Logging/Handlers/FileLoggerHandler.cs
@@ -36,7 +36,7 @@ namespace FLog.Logging.Handlers
                     directoryInfo.Create();
             }
 
-            using (var writer = new StreamWriter(File.Open(Path.Combine(_directory, _fileName), FileMode.Append)))
+            using (var writer = new StreamWriter(File.Open(Path.Combine(_directory, _fileName), FileMode.Append, FileAccess.Write, FileShare.ReadWrite)))
                 writer.WriteLine(_loggerFormatter.ApplyFormat(logMessage));
         }
 
